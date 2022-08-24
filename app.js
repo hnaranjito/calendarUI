23f359235fec68a1e60a94ba7be599a78e56b985
Hector Naranjo Gallego <h.naranjo@utp.edu.co>
2022-08-24 00:10:06 -0500

    Carga inicial

---
+++ app.js
@@ -0,0 +1,78 @@
+//DARK MODE TOGGLE
+let dark_mode_toggle = document.querySelector('.dark-mode-switch')
+
+dark_mode_toggle.onclick = () => {
+    document.querySelector('body').classList.toggle('light')
+    document.querySelector('body').classList.toggle('dark')
+}
+
+//CHECK LEAP YEAR
+
+isLeapYear = (year) => {
+        return (year % 4 === 0 && year % 100 !== 0 && year % 400 !== 0) 
+            || (year % 100 === 0 && year % 400 === 0 );
+}
+
+getfebDays = (year) => {
+    return isLeapYear(year) ? 29 : 28;
+}
+
+let calendar = document.querySelector('.calendar');
+
+const month_names = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+
+let month_picker = document.querySelector('#month-picker');
+
+month_picker.onclick = () => {
+    month_list.classList.add('show');
+}
+
+//GENERATE CALENDAR
+
+generateCalendar = (month, year) => {
+    let calendar_days = document.querySelector('.calendar-days');
+    let calendar_header_year = document.querySelector('#year');
+    
+    let days_of_month = [31, getfebDays(year), 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+    
+    calendar_days.innerHTML = '';
+    
+    let currDate = new Date();
+    month_picker.innerHTML = month_names[month];
+    calendar_header_year.innerHTML = year;
+
+    let first_day = new Date(month, year, 1);
+
+    for(let i=0; i <= days_of_month[month] + first_day.getDay() - 1; i++){
+        let day = document.createElement('div');
+        if(i>= first_day.getDay()){
+            day.classList.add('calendar-day-hover');
+            day.innerHTML = i - first_day.getDay() + 1;
+            day.innerHTML += `<span><span>
+                            <span><span>
+                            <span><span>
+                            <span><span>
+                            `;
+            if(i - first_day.getDay() + 1 === currDate.getDate() && year === currDate.getFullYear() && 
+                month === currDate.getMonth()) {
+                    day.classList.add('curr-date');
+                }
+        }
+        calendar_days.appendChild(day);
+    }
+}
+
+let month_list = calendar.querySelector('.month-list');
+
+month_names.forEach((e, index)=>{
+    let month = document.createElement('div');
+    month.innerHTML = `<div>${e}</div>`;
+    month_list.appendChild(month);
+});
+
+let currDate = new Date()
+
+let curr_month = {value: currDate.getMonth()}
+let curr_year = {value: currDate.getFullYear()}
+
+generateCalendar(curr_month.value, curr_year.value);
